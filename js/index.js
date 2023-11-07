a2c3f1d0bb20d78d8ea21621d749dc619533a2d4
Ivan Shchururpov <vanya.schurupov@yandex.ru>
2023-11-07 11:47:08 +0300

    rewiev code

--- js/index.js
+++ js/index.js
@@ -1,57 +1,65 @@
-new Swiper('.swiper', {
-    navigation: {
-        nextEl: '.portfolio__arrow-right',
-        prevEl: '.portfolio__arrow-left',
-        disabledClass: 'portfolio__arrow-desable',
+new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".portfolio__arrow-right",
+    prevEl: ".portfolio__arrow-left",
+    disabledClass: "portfolio__arrow-desable",
+  },
+  breakpoints: {
+    320: {
+      grid: {
+        fill: "row",
+        rows: 2,
+      },
     },
-    breakpoints: {
-        320: {
-            grid: {
-                fill: 'row',
-                rows: 2
-            }
-        },
-        768: {
-            slidesPerView: 2,
-            grid: {
-                fill: 'row',
-                rows: 2
-            }
-        },
-
+    768: {
+      slidesPerView: 2,
+      grid: {
+        fill: "row",
+        rows: 2,
+      },
     },
-    autoplay: {
-        delay: 5000,
+  },
+  //   autoplay: {
+  //     delay: 5000,
+  //   },
+});
+
+$(".header__contacts-burger").click(() => {
+  $(".header__contacts").slideToggle();
+  $(".callback__label").focus();
+});
+
+// $(".preset__order-btn").click(() => {
+//   $(".page__overlay-modal").fadeIn(400).css("display", "flex");
+// });
+
+$(".preset__order-btn").click(() => {
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#portfolio").offset().top,
     },
+    1000
+  );
 });
 
-$('.header__contacts-burger').click(() => {
-    $('.header__contacts').slideToggle();
-    $('.callback__label').focus();
+$(".modal__close").click(() => {
+  $(".page__overlay-modal").fadeOut(400);
 });
 
-$('.preset__order-btn').click(() => {
-    $('.page__overlay-modal').fadeIn(400).css('display', 'flex');
+$(".page__overlay-modal").click((e) => {
+  if (e.target.classList.contains("page__overlay-modal")) {
+    $(".page__overlay-modal").fadeOut(400);
+  }
 });
 
-$('.modal__close').click(() => {
-    $('.page__overlay-modal').fadeOut(400);
+$(".callback__btn").click((e) => {
+  e.preventDefault();
+  $(".page__overlay-modal").fadeOut(400);
 });
 
-$('.page__overlay-modal').click((e) => {
-    if(e.target.classList.contains('page__overlay-modal')){
-        $('.page__overlay-modal').fadeOut(400);
-    }
+elem = document.querySelector(".present__photo-btn");
+elem.addEventListener("click", () => {
+  $(".page__overlay-modal").fadeIn(400).css("display", "flex");
 });
 
-$('.callback__btn').click((e) => {
-    e.preventDefault();
-        $('.page__overlay-modal').fadeOut(400);
-});
-
-elem = document.querySelector('.present__photo-btn');
-console.log(elem);
-elem.addEventListener('click', () => {
-    $('.page__overlay-modal').fadeIn(400).css('display', 'flex');
-    
-})
+Fancybox.bind('[data-fancybox="gallery"]', {});
